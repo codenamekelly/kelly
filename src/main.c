98370896ec84f95be87f05d094b0982a2442491b
unknown <Nick@.(none)>
2011-06-06 19:05:53 +1000

    Add changes to main.c for SDL event loop and opengl

--- src/main.c
+++ src/main.c
@@ -1,39 +1,47 @@
-/*
-  Copyright (C) 1997-2011 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely.
-*/
-
 #include <stdio.h>
-#define SDL_NO_COMPAT
+#include <stdlib.h>
 
-#include <SDL\SDL.h>
+#include "SDL.h"
+#include "SDL_opengl.h"
+
 int main( int argc, char **argv )
 {
-	SDL_Window *win = NULL;
-	int width = 320, height = 240;
-
-	if (SDL_Init(SDL_INIT_VIDEO)) {
-		printf("ERROR: SDL_INIT_VIDEO filed\n");
-	return -1;
+	if( SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+		printf("Unable to initialize SDL: %s\n", SDL_GetError());
+		return 1;
 	}
-	printf("INFO : win = %p\n", win); // printing (nil)
-
-	win = SDL_CreateWindow ("Hello World", 10, 10, width, height, SDL_WINDOW_RESIZABLE);
-	if(win == NULL) {
-		printf("ERROR: SDL_CreateWindow Failed\n");
-		return -1;
+	
+	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+	SDL_Surface* screen = SDL_SetVideoMode( 640, 480, 16, SDL_OPENGL );
+	
+	glEnable( GL_TEXTURE_2D );
+	
+	glClearColor( 1.0f, 1.0f, 0.0f, 1.0f );
+ 
+	glViewport( 0, 0, 640, 480 );
+	 
+	glClear( GL_COLOR_BUFFER_BIT );
+	 
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	 
+	glOrtho(0.0f, 640, 480, 0.0f, -1.0f, 1.0f);
+	 
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+	
+	BOOL running = TRUE;
+	SDL_Event event;
+	
+	while(running) {
+		while(SDL_PollEvent(&event)) {
+			switch(event.type){
+				case SDL_QUIT:
+					running = 0;
+					break;
+			}
+		}
 	}
-	printf("INFO : SDL_CreateWindow Success win = %p\n", win);
-
-	SDL_ShowWindow(win);
-	SDL_DestroyWindow(win);
-
+	
 	return 0;
 } 
