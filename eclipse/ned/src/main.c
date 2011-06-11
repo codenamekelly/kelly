63146ee49fce73c4aba129fe7bd80fb4e1aa41bf
Andrew Naish <anaish@lycos.com>
2011-06-11 14:13:15 +1000

    added sample eclipse project

---
+++ eclipse/ned/src/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_opengl.h"
+
+int main( int argc, char **argv )
+{
+	if( SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+		printf("Unable to initialize SDL: %s\n", SDL_GetError());
+		return 1;
+	}
+	
+	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+	SDL_Surface* screen = SDL_SetVideoMode( 640, 480, 16, SDL_OPENGL );
+	
+	glEnable( GL_TEXTURE_2D );
+	
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+ 
+	glViewport( 0, 0, 640, 480 );
+	 
+	glClear( GL_COLOR_BUFFER_BIT );
+	 
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	 
+	glOrtho(0.0f, 640, 480, 0.0f, -1.0f, 1.0f);
+	 
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+	
+	BOOL running = TRUE;
+	SDL_Event event;
+	
+	while(running) {
+		while(SDL_PollEvent(&event)) {
+			switch(event.type){
+				case SDL_QUIT:
+					running = 0;
+					break;
+			}
+		}
+	}
+	
+	return 0;
+} 
